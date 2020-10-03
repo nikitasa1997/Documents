7a5ee04fc3174305a9ee1795fc793b1c2142a08d
Nikita <nikitasa1997@gmail.com>
2020-10-03 11:32:00 +0300

    Add files via upload

--- Windows 10/new_services.py
+++ Windows 10/new_services.py
@@ -4,14 +4,15 @@ import re
 def callable_or_raise(obj):
     if not callable(obj):
         raise TypeError(type(obj))
+        raise TypeError(F"'{type(obj)}' object is not callable")
 
 def isinstance_or_raise(obj, obj_type):
     if not isinstance(obj, obj_type):
-        raise TypeError(type(obj))
+        raise TypeError(F"value {obj} has type '{type(obj)}'")
 
 def len_or_raise(s):
     if not len(s):
-        raise ValueError(len(s))
+        raise ValueError('zero len')
 
 def startmode_to_dword(mode):
     isinstance_or_raise(mode, str)
@@ -34,7 +35,7 @@ def get_session_key(records, get_name):
         if findall:
             count[findall[0]] = count.get(findall[0], 0) + 1
     if not len(count):
-        return None
+        return
     return max(count, key = count.get)
 
 def replace(compile_key, pattern, repl, string):
@@ -49,20 +50,20 @@ def replace(compile_key, pattern, repl, string):
         return string[ : -len(pattern)] + repl
     return string
 
-default_session_key = '_00000000'
+DEFAULT_SESSION_KEY = '_00000000'
 
-cmd_encoding = 'utf-16'
+CMD_ENCODING = 'utf-16'
 
-csv_header = 1
-csv_record = 2
+CSV_HEADER = 1
+CSV_RECORD = 2
 
 def csv_to_dict(csv_file):
-    with open(csv_file, 'r', encoding = cmd_encoding) as file:
+    with open(csv_file, 'r', encoding=CMD_ENCODING) as file:
         reader = csv.reader(file)
         lines = [tuple(line) for line in reader]
-        header = lines[csv_header]
-        header = { header[i] : i for i in range(len(header)) }
-        records = lines[csv_record : ]
+        header_line = lines[CSV_HEADER]
+        header = { header_line[i] : i for i in range(len(header_line)) }
+        records = lines[CSV_RECORD : ]
 
         session_key = get_session_key(records, lambda record: record[header['Name']])
         name_startmode = dict()
@@ -73,54 +74,54 @@ def csv_to_dict(csv_file):
             if session_key and compile_key.match(name):
                 name = name[ : -len(session_key)]
                 name_startmode[name] = startmode
-                name += default_session_key
+                name += DEFAULT_SESSION_KEY
             name_startmode[name] = startmode
     return name_startmode, session_key
 
-title = 'Windows Registry Editor Version 5.00\r\n'
+TITLE = 'Windows Registry Editor Version 5.00\r\n'
 line_0 = '[HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\{}]\r\n'
-line_1 = '\"Start\"=dword:0000000{}\r\n'
+line_1 = '"Start"=dword:0000000{}\r\n'
 
-default_encoding = 'utf-8'
+DEFAULT_ENCODING = 'utf-8'
 
 def dict_to_reg(name_startmode, session_key, reg_file):
-    with open(reg_file, 'w', encoding = default_encoding) as file:
-        file.write(title + '\r\n')
+    with open(reg_file, 'w', encoding = DEFAULT_ENCODING) as file:
+        file.write(TITLE + '\r\n')
         key = lambda s: s[0].casefold()
-        compile_key = re.compile(default_session_key + '$')
+        compile_key = re.compile(DEFAULT_SESSION_KEY + '$')
         for name, startmode in sorted(name_startmode.items(), key = key):
             if compile_key.match(name):
-                name = name[ : -len(default_session_key)] + session_key
+                name = name[ : -len(DEFAULT_SESSION_KEY)] + session_key
             file.write(line_0.format(name) + line_1.format(startmode) + '\r\n')
 
-reg_title = 2
+REG_TITLE = 2
 
 start = 0
 end = 1
 
-offset_0 = {
+OFFSET_0 = {
     start : len('[HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\'),
     end : -len(']\n')
 }
 
-offset_1 = {
-    start : len('\"Start\"=dword:0000000'),
+OFFSET_1 = {
+    start : len('"Start"=dword:0000000'),
     end : -len('\n')
 }
 
 def reg_to_dict(reg_file):
     name_startmode = dict()
-    with open(reg_file, 'r', encoding = default_encoding) as file:
-        lines = file.readlines()[reg_title : ]
+    with open(reg_file, 'r', encoding=DEFAULT_ENCODING) as file:
+        lines = file.readlines()[REG_TITLE : ]
         for i in range(0, len(lines), 3):
-            name = lines[i][offset_0[start] : offset_0[end]]
-            startmode = int(lines[i + 1][offset_1[start] : offset_1[end]])
+            name = lines[i][OFFSET_0[start] : OFFSET_0[end]]
+            startmode = int(lines[i + 1][OFFSET_1[start] : OFFSET_1[end]])
             name_startmode[name] = startmode
     session_key = get_session_key(name_startmode, lambda name: name)
     compile_key = re.compile(session_key + '$') if session_key else None
     def replace(compile_key, pattern, repl, string):
     if session_key:
-        name_startmode = { replace(compile_key, session_key, default_session_key, name) :
+        name_startmode = { replace(compile_key, session_key, DEFAULT_SESSION_KEY, name) :
             name_startmode[name] for name in name_startmode }
     return name_startmode
 
@@ -138,9 +139,9 @@ if __name__ == "__main__":
     name_startmode_reg = reg_to_dict('test.reg')
     print(name_startmode_csv == name_startmode_reg)
 """
-    if default_session_key in name:
-        name = re.sub(default_session_key + '$', session_key, name, count = 1)
+    if DEFAULT_SESSION_KEY in name:
+        name = re.sub(DEFAULT_SESSION_KEY + '$', session_key, name, count = 1)
 
     count = 1 ?????????
-    Нужно ли проверять default_session_key in name. Или можно применить name = ... для всех?
+    Нужно ли проверять DEFAULT_SESSION_KEY in name. Или можно применить name = ... для всех?
     pass
