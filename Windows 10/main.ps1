ddfff52f80ace5c324f6ee960ab82f9cfa893258
Nikita <nikitasa1997@gmail.com>
2020-10-04 22:56:44 +0300

    Add files via upload

--- Windows 10/main.ps1
+++ Windows 10/main.ps1
@@ -2,3 +2,7 @@ $Service = Get-Service | Select Name, StartType | Group-Object -AsHashtable Name
 $Service = Get-Service | Where ServiceType -eq 96 | Select Name, StartType
 $Service = Get-Service | Where ServiceType -in (224, 240) | Select Name, StartType | Group-Object -AsHashtable
 $Service | Select Name | -replace '^.+?_', '' | Sort-Object -Unique
+
+
+function svcsuffix `%@word["_",1,%@execstr[wmiquery /a . "select Name from Win32_Service" | ffind /k /m /e"_[0-9a-f]+$"]]`
+echo %@svcsuffix[]
