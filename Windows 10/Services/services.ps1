67cb8fba3337b906eda80e4c07d88aefa64c7237
Nikita <nikitasa1997@gmail.com>
2020-10-06 22:19:43 +0300

    next

--- Windows 10/Services/services.ps1
+++ Windows 10/Services/services.ps1
@@ -1,34 +1,41 @@
 Set-StrictMode -Version Latest
 
+$Encoding = 'utf8'
 $DefaultLUID = '00000000'
 
-$Path = '\Users\nikit\Downloads\Documents\Windows 10\Services\services.json'
-$Encoding = 'utf8'
+function ServiceTo-Json {
+    Param(
+        [string]$Path
+    )
+    process {
+        $Service = Get-Service |
+            Select-Object -Property Name, ServiceType, StartType
+        $PerUserServices = $Service |
+            Where-Object -Property ServiceType -In -Value @(224, 240) |
+            Select-Object -ExpandProperty Name
+        $LUID = $PerUserServices |
+            Foreach-Object -Process {$_ -replace '^.+_([0-9a-f]{4,8})$', '$1'} |
+            Sort-Object -Unique
+        $Service += $PerUserServices |
+            Foreach-Object -Process {$_ -replace "^(.+)_$LUID`$", '$1'} |
+            Get-Service |
+            Select-Object -Property Name, StartType
 
-$Service = Get-Service |
-    Select-Object -Property Name, ServiceType, StartType
-$PerUserServices = $Service |
-    Where-Object -Property ServiceType -In -Value @(224, 240) |
-    Select-Object -ExpandProperty Name
-$LUID = $PerUserServices |
-    Foreach-Object -Process {$_ -replace '^.+_([0-9a-f]{4,8})$', '$1'} |
-    Sort-Object -Unique
-$Service += $PerUserServices |
-    Foreach-Object -Process {$_ -replace "^(.+)_$LUID`$", '$1'} |
-    Get-Service |
-    Select-Object -Property Name, StartType
+        $Service |
+            Sort-Object -Property Name |
+            Foreach-Object -Begin {
+                $Ordered = [ordered]@{}
+            } -Process {
+                $Ordered.Add(
+                    ($_.Name -replace "^(.+)_$LUID`$", "`$1_$DefaultLUID"),
+                    $_.StartType
+                )
+            } -End {
+                $Ordered
+            } |
+            ConvertTo-Json -Depth 1 |
+            Set-Content -Path $Path -Encoding $Encoding
+    }
+}
 
-$Service |
-    Sort-Object -Property Name |
-    Foreach-Object -Begin {
-        $Ordered = [ordered]@{}
-    } -Process {
-        $Ordered.Add(
-            ($_.Name -replace "^(.+)_$LUID`$", "`$1_$DefaultLUID"),
-            $_.StartType
-        )
-    } -End {
-        $Ordered
-    } |
-    ConvertTo-Json -Depth 1 |
-    Set-Content -Path $Path -Encoding $Encoding
+ServiceTo-Json('\Users\nikit\Downloads\Documents\Windows 10\Services\services.json')
