a9072d1b4c5e11ec847c950ba9e5b1c139787236
Nikita <nikitasa1997@gmail.com>
2020-10-06 15:07:33 +0300

    update

--- Windows 10/Services/services.ps1
+++ Windows 10/Services/services.ps1
@@ -1,15 +1,24 @@
 Set-StrictMode -Version Latest
 
+$DefaultLUID = '00000000'
+
 $Path = '\Users\nikit\Downloads\services.json'
 $Encoding = 'utf8'
 
 $Service = Get-Service |
     Select-Object -Property Name, ServiceType, StartType
-$LUID = $Service |
+
+$PerUserServices = $Service |
     Where-Object -Property ServiceType -in (224, 240) |
-    Select-Object -ExpandProperty Name |
+    Select-Object -ExpandProperty Name
+$LUID = $PerUserServices |
     Foreach-Object -Process {$_ -replace '^.+_([0-9a-f]{4,8})$', '$1'} |
     Sort-Object -Unique
+$Service += $PerUserServices |
+    Foreach-Object -Process {$_ -replace "^(.+)_$LUID`$", '$1'} |
+    Get-Service |
+    Select-Object -Property Name, StartType
+
 $Service |
     Foreach-Object -Begin {
         $Ordered = [ordered]@{}
